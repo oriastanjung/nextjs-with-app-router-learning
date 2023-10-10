9bd2397bac35f028010bf61c6266aa2473dba290
oriastanjung <oriastan999@gmail.com>
2023-10-10 10:59:55 +0700

    learning the usage of client component

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -1,4 +1,6 @@
 import Heading from "@/components/Heading";
+import { ShareButtons } from "@/components/ShareButtons";
+import  ShareLinkButton from "@/components/ShareLinkButton";
 import { getReview, getSlugs } from "@/lib/reviews";
 
 export async function generateStaticParams() {
@@ -6,20 +8,25 @@ export async function generateStaticParams() {
   return slugs.map((slug) => ({ slug: slug }));
 }
 
-// dynamic metadata by its params 
-export async function generateMetadata(props){
+// dynamic metadata by its params
+export async function generateMetadata(props) {
   const review = await getReview(props.params.slug);
   return {
-    title : review.title
-  }
+    title: review.title,
+  };
 }
 
 export default async function ReviewPage(props) {
   const review = await getReview(props.params.slug);
-
+  
   return (
     <>
       <Heading>{review.title}</Heading>
+      <div className="flex gap-3 items-center">
+        <p className="italic pb-2">{review.date}</p>
+        <ShareLinkButton />
+        {/* <ShareButtons /> */}
+      </div>
       <img
         src={review.image}
         alt=""
---
+++ components/ShareButtons.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import  ShareLinkButton  from "./ShareLinkButton";
+
+export const ShareButtons = () => {
+    // console.log("[ShareButtons] rendering")
+  return (
+    <div>
+      <ShareLinkButton /> | [Twitter] | [Facebook]
+    </div>
+  );
+};
